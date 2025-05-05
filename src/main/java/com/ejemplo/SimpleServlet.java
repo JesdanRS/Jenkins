37def7e8e93bd7495a417fa75ef6888ef93aee24
JesdanRS <imajesus08@gmail.com>
2025-05-05 08:02:52 -0400

    correccion de errores en backend

--- src/main/java/com/ejemplo/SimpleServlet.java
+++ src/main/java/com/ejemplo/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println(<title>Aplicación Simple para Jenkins y Tomcat</title>");
+            out.println("<title>Aplicación Simple para Jenkins y Tomcat</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>¡Hola desde mi aplicación Java desplegada con Jenkins en Tomcat!</h1>");
